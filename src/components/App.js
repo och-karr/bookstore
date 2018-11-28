86faa97370f843a9ad2d3af9a494a1e1d2539fb8
karolinneczka <okaa93@gmail.com>
2018-11-28 18:07:30 +0100

    Create app with create-react-app, first component - App - and first tests

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'; //React - nazwa importowanego obiektu z pakietu 'react'
+
+class App extends React.Component {
+    render() {
+        return <div><i>Hello World</i></div>
+    }
+}
+
+export default App;
